ef2742176b2aeddb4808e80cfcd63c16cb0987d0
Rômulo Cesário <romuloccgvagas@gmail.com>
2026-02-25 14:45:34 -0300

    Update create_trigger.sql

--- scripts/create_trigger.sql
+++ scripts/create_trigger.sql
@@ -1,1 +1,16 @@
+CREATE TRIGGER CalculaFaturamentoDiario
+AFTER INSERT ON itenspedidos
+FOR EACH ROW 
+BEGIN
+DELETE FROM Faturamentodiario;
+INSERT INTO Faturamentodiario (Dia, faturamentototal)
+SELECT DATE(datahorapedido) AS Dia, SUM(ip.precounitario) as FaturamentoDiario 
+from pedidos p
+join itenspedidos ip
+on p.id = ip.idpedido
+GROUP BY Dia
+ORDER BY Dia;
+END;
 
+--PARA VERIFICAR FUNCIONAMENTO
+SELECT * from Faturamentodiario;
