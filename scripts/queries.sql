031395881da1e79535cc04d03cd2b1e78a7677df
Rômulo Cesário <romuloccgvagas@gmail.com>
2026-02-25 13:13:08 -0300

    Update queries.sql

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,1 +1,41 @@
+-- SUBSCONSUTAS E CONSULTAS SIMPLES
+SELECT * FROM clientes;
 
+
+SELECT * FROM pedidos;
+
+
+SELECT idcliente FROM pedidos WHERE datahorapedido = '2023-01-02 08:15:00';
+
+
+SELECT nome
+FROM clientes
+WHERE id = (SELECT idcliente FROM pedidos WHERE datahorapedido = '2023-01-02 08:15:00');
+
+
+-- SUBSCONSUTAS COM IN E HAVING
+SELECT idcliente
+FROM pedidos
+WHERE strftime('%m', datahorapedido) = '01' ;
+
+
+SELECT nome
+FROM clientes
+WHERE id IN (
+    SELECT idcliente
+    FROM pedidos
+    WHERE strftime('%m', datahorapedido) = '01');
+
+
+SELECT AVG(preco) FROM produtos;
+
+
+SELECT nome, preco
+FROM produtos
+HAVING preco > (SELECT AVG(preco) FROM produtos);
+
+
+SELECT nome, preco
+FROM produtos
+GROUP BY nome, preco
+HAVING preco > (SELECT AVG(preco) FROM produtos);
