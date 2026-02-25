19a3df3e2434a6de634691e7c2532811bfdd4c71
Rômulo Cesário <romuloccgvagas@gmail.com>
2026-02-25 14:48:35 -0300

    Update create_trigger.sql

--- scripts/create_trigger.sql
+++ scripts/create_trigger.sql
@@ -14,3 +14,28 @@ END;
 
 --PARA VERIFICAR FUNCIONAMENTO
 SELECT * from Faturamentodiario;
+
+
+-- INSERINDO PARA ACIONAR TRIGGER
+SELECT * from Faturamentodiario;
+
+
+INSERT INTO Pedidos (ID, IDCliente, DataHoraPedido, Status)
+VALUES (451, 27, '2023-10-07 14:30:00', 'Em Andamento');
+
+
+INSERT INTO ItensPedidoS
+(IDPedido, IDProduto, Quantidade, PrecoUnitario)
+VALUES(451, 14, 1, 6.0),
+      (451, 13, 1, 7.0);
+
+
+
+INSERT INTO Pedidos (ID, IDCliente, DataHoraPedido, Status)
+VALUES (452, 28, '2023-10-07 14:35:00', 'Em Andamento');
+
+
+INSERT INTO ItensPedidos
+(IDPedido, IDProduto, Quantidade, PrecoUnitario)
+VALUES(452, 10, 1, 5.0),
+      (452, 31, 1, 12.50);
