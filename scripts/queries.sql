cb6dcce2d56ab7bbc49511ec15b35fba611fc0c4
Rômulo Cesário <romuloccgvagas@gmail.com>
2026-02-25 13:47:59 -0300

    Update queries.sql

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -59,3 +59,47 @@ SELECT nome, rua, bairro, cidade, estado, cep FROM colaboradores
 UNION ALL
 SELECT nome, rua, bairro, cidade, estado, cep FROM fornecedores;
 
+
+-- LEFT JOIN, FULL JOIN, USO DE ALIAS
+ SELECT c.nome, p.id
+ FROM clientes c 
+ LEFT JOIN Pedidos p 
+ ON c.id = p.idcliente;
+ 
+
+SELECT c.nome, P.idcliente 
+ FROM clientes c 
+ LEFT JOIN Pedidos p 
+ ON c.id = p.idcliente
+WHERE strftime('%m', p.datahorapedido) = '10' ;
+
+
+SELECT c.nome, x.id 
+FROM clientes c
+LEFT JOIN
+(
+	SELECT p.id, p.idcliente 
+	FROM pedidos p
+	WHERE strftime('%m', p.datahorapedido) = '09')x
+ON c.id = x.idcliente
+WHERE x.idcliente is NULL;
+
+
+SELECT c.nome, p.id 
+FROM clientes c
+FULL JOIN pedidos p
+ON c.id = p.idcliente;
+
+
+SELECT c.nome, p.id 
+FROM clientes c
+FULL JOIN pedidos p
+ON c.id = p.idcliente
+WHERE c.id is NULL;
+
+
+SELECT c.nome, p.id 
+FROM clientes c
+FULL JOIN pedidos p
+ON c.id = p.idcliente
+WHERE p.id is NULL;
